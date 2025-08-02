05c55d95d637242a3254fbed21d3ef57960dddf5
miracle boniface <udohmiracle23@gmail.com>
2025-08-02 12:29:40 +0100

    repo updated

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'https://tradephere-backend.vercel.app/'
+const baseURL = 'https://tradephere-backend.vercel.app/api'
 // const baseURL = 'http://localhost:5000/api'
 
 const api = axios.create({
