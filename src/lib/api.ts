c8280fca41bcfe464b7a03ce33a75bdf8b067713
miracle boniface <udohmiracle23@gmail.com>
2025-08-02 12:43:41 +0100

    repo updated

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseURL = 'https://tradephere-backend.vercel.app/api'
 // const baseURL = 'http://localhost:5000/api'
+const baseURL = 'https://tradephere-backend.vercel.app/api/'
 
 const api = axios.create({
   baseURL, // your NestJS backend
